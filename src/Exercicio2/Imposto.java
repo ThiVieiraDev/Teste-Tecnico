79dd472e9856751da231d39f3da8013fd01950be
Thi Vieira <thiiagoviieira@gmail.com>
2021-02-04 15:11:50 -0300

    Classe VideoGame criada herdando de Produto e seus respectivos construtores e Getters and Setters

---
+++ src/Exercicio2/Imposto.java
@@ -0,0 +1,6 @@
+package Exercicio2;
+
+public abstract interface Imposto {
+
+
+}
---
+++ src/Exercicio2/VideoGame.java
@@ -0,0 +1,41 @@
+package Exercicio2;
+
+// Classe VideoGame herdando de Produto
+public class VideoGame extends Produto{
+    private String marca;
+    private String modelo;
+    private boolean isUsado;
+
+    // Construtores
+    public VideoGame() {
+    }
+    public VideoGame(String nome, double preco, int qtd, String marca, String modelo, boolean isUsado) {
+        super(nome, preco, qtd);
+        this.marca = marca;
+        this.modelo = modelo;
+        this.isUsado = isUsado;
+    }
+
+    // Getters and Setters
+    public String getMarca() {
+        return marca;
+    }
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public boolean isUsado() {
+        return isUsado;
+    }
+    public void setUsado(boolean usado) {
+        isUsado = usado;
+    }
+
+}
